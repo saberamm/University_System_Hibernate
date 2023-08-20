12a01ef6dfec6c1ca39efa73606808df1d7c40e0
saber <sbaerm.com@gmail.com>
2023-08-21 01:18:30 +0330

    add user repo to project

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import base.repository.BaseRepository;
+import entity.User;
+
+public interface UserRepository extends BaseRepository<User, Long> {
+    User findUserByUsername(String username);
+}
---
+++ src/main/java/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,30 @@
+package repository.impl;
+
+import base.repository.impl.BaseRepositoryImpl;
+import entity.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import repository.UserRepository;
+
+public class UserRepositoryImpl extends BaseRepositoryImpl<User, Long> implements UserRepository {
+
+    public UserRepositoryImpl(EntityManager entityManager) {
+        super(entityManager);
+    }
+
+    @Override
+    public Class<User> getEntityClass() {
+        return User.class;
+    }
+
+    @Override
+    public User findUserByUsername(String username) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
+        Root<User> root = criteriaQuery.from(User.class);
+        criteriaQuery.select(root).where(builder.equal(root.get("username"), username));
+        return entityManager.createQuery(criteriaQuery).getSingleResult();
+    }
+}
