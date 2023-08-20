f763bc0cea82dc84aa8bd3791657d0cc3706c8e7
saber <sbaerm.com@gmail.com>
2023-08-21 01:17:43 +0330

    add teacher entity to project

---
+++ src/main/java/entity/Teacher.java
@@ -0,0 +1,65 @@
+package entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Teacher extends User {
+    @Column(name = "teacher_number")
+    private String teacherNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Course> courses;
+
+    public Teacher(String teacherNumber, List<Course> courses) {
+        this.teacherNumber = teacherNumber;
+        this.courses = courses;
+    }
+
+    public Teacher(String firstName, String lastName, String username, String password, LocalDate birthDate, String teacherNumber, List<Course> courses) {
+        super(firstName, lastName, username, password, birthDate);
+        this.teacherNumber = teacherNumber;
+        this.courses = courses;
+
+    }public Teacher(String firstName, String lastName, String username, String password, LocalDate birthDate, String teacherNumber) {
+        super(firstName, lastName, username, password, birthDate);
+        this.teacherNumber = teacherNumber;
+    }
+
+    public Teacher() {
+    }
+
+    public String getTeacherNumber() {
+        return teacherNumber;
+    }
+
+    public void setTeacherNumber(String teacherNumber) {
+        this.teacherNumber = teacherNumber;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + getId() +
+                ",  teacherNumber='" + teacherNumber + '\'' +
+                ", courses=" + courses +
+                ", firstName='" + getFirstName() + '\'' +
+                ", lastName='" + getLastName() + '\'' +
+                ", username='" + getUsername() + '\'' +
+                ", password='" + getPassword() + '\'' +
+                ", birthDate=" + getBirthDate() +
+                '}';
+    }
+}
