b412f2466e8ad8e0aa09c0858a09104697df835a
saber <sbaerm.com@gmail.com>
2023-08-21 01:18:48 +0330

    add user service to project

---
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package service.impl;
+
+import base.service.impl.BaseServiceImpl;
+import dto.DtoMapper;
+import entity.User;
+import entity.dto.SimpleUser;
+import repository.UserRepository;
+import service.UserService;
+
+public class UserServiceImpl extends BaseServiceImpl<User, Long, UserRepository> implements UserService {
+
+    public DtoMapper dtoMapper;
+
+    public UserServiceImpl(UserRepository repository, DtoMapper dtoMapper) {
+        super(repository);
+        this.dtoMapper = dtoMapper;
+    }
+
+    @Override
+    public SimpleUser findUserByUsername(String username) {
+        return dtoMapper.userDtoMapper(repository.findUserByUsername(username));
+    }
+}
